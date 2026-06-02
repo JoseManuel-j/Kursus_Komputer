3a1fa6aba232f14df6dd0431380a47124fca9cb5
Josyua <yosuaranantama@email.com>
2026-06-02 20:55:59 +0700

    Mengupdate file yang tertinggal

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProgramController; // <-- Jangan lupa import controllernya
+use App\Http\Controllers\Api\InstrukturController;
 
 // Ini rute default bawaan Laravel buat ngecek data user login (pakai token)
 Route::get('/user', function (Request $request) {
@@ -13,3 +14,4 @@ Route::get('/user', function (Request $request) {
 
 // Endpoint buat ngambil daftar 28 kelas kursus
 Route::get('/program-kursus', [ProgramController::class, 'index']);
+Route::get('/instruktur/jadwal/{id}', [InstrukturController::class, 'getJadwal']);
